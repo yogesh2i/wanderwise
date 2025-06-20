843bdf2dd508686cde2e74367265933bbf21cdf4
yogesh <yogirao8440@gmail.com>
2025-06-20 11:27:37 +0530

    new: token api added

---
+++ src/app/api/checkAuth/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from 'next/server';
+import { authenticate } from '@/utility/authenticate'; 
+import { cookies } from 'next/headers';
+export async function GET() {
+  try {
+    const token = (await cookies()).get('token')?.value || '';
+
+    if (!token) {
+      return NextResponse.json(
+        { success: false, error: 'Unauthorized: No token provided' },
+        { status: 401 } 
+      );
+    }
+
+    const isValid = await authenticate(token); 
+
+    if (!isValid) {
+      return NextResponse.json(
+        { success: false, error: 'Unauthorized: Invalid token' },
+        { status: 401 } 
+      );
+    }
+
+    return NextResponse.json(
+      { success: true, message: 'User is authenticated' },
+      { status: 200 } 
+    );
+  } catch (error) {
+    console.error('Error validating token:', error);
+    return NextResponse.json(
+      { success: false, error: 'Internal server error' },
+      { status: 500 }
+    );
+  }
+}
