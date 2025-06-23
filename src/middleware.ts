4e7a4f7e61967afb9ad98c28b83cc69ddfc10afc
yogesh <yogirao8440@gmail.com>
2025-06-23 09:49:11 +0530

    update: next auth live now

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,50 @@
+import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
-import { authenticate } from './utility/authenticate';
+// import { authenticate } from './utility/authenticate';
+// import { cookies } from 'next/headers';
 
-export async function middleware(req: NextRequest) {
-    const token = req.cookies.get('token')?.value || '';
-    if (token === '' && req.nextUrl.pathname === "/login") {
-        return NextResponse.next();
-    }
+// export async function middleware(req: NextRequest) {
+//     const token = req.cookies.get('token')?.value || '';
+//     if (token === '' && req.nextUrl.pathname === "/login") {
+//         return NextResponse.next();
+//     }
 
-    if (token === '' && req.nextUrl.pathname !== "/login") {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
+//     if (token === '' && req.nextUrl.pathname !== "/login") {
+//         return NextResponse.redirect(new URL("/login", req.url));
+//     }
      
-    const isValid: boolean = await authenticate(token);
+//     const isValid: boolean = await authenticate(token);
 
-    if(isValid && req.nextUrl.pathname === "/login") {
-        return NextResponse.redirect(new URL("/",req.url));
-    }
+//     if(isValid && (req.nextUrl.pathname === "/login" || req.nextUrl.pathname==="/signup")) {
+//         return NextResponse.redirect(new URL("/",req.url));
+//     }
 
-    if (!isValid && req.nextUrl.pathname !== "/login") {
-        return NextResponse.redirect(new URL("/login", req.url));
-    }
+//     if (!isValid && req.nextUrl.pathname !== "/login") {
+//         return NextResponse.redirect(new URL("/login", req.url));
+//     }
 
    
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ['/plan-trip', '/booking', '/chat','/login','/signup'],
+// };
+
+export async function middleware(req: NextRequest) {
+     const token =  await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+     const url = req.nextUrl;
+     if(token && (url.pathname==='/logIn')){
+        return NextResponse.redirect(new URL("/",req.url));
+     }
+   
+    if (!token && url.pathname !== '/logIn') {
+        return NextResponse.redirect(new URL('/logIn', req.url));
+    }
     return NextResponse.next();
+
 }
-
 export const config = {
-    matcher: ['/plan-trip', '/booking', '/chat','/login'],
+    matcher: ['/plan-trip', '/booking', '/chat', '/signup','/logIn'],
 };
-
