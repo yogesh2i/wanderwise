eaa51055d43051aab5898f1c74feb7227be9932c
yogesh <yogirao8440@gmail.com>
2025-06-23 09:50:37 +0530

    update: updated links with next auth

--- src/components/home/navbar/NavLinks.tsx
+++ src/components/home/navbar/NavLinks.tsx
@@ -1,32 +1,34 @@
 "use client";
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import React, { useState } from 'react'
 
-interface Logged {
-    isLoggedIn: string
-}
+// interface Logged {
+//     _isLoggedIn: string
+// }
 
-export default function NavLinks({isLoggedIn}: Logged) {
-
+export default function NavLinks() {
+    const {status} = useSession();
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const handleLinkClick = () => {
       setIsMenuOpen(false);
     };
     const handleLogout = ()=>{
-    
+        
         handleLinkClick();
-        fetch('/api/logout')
-            .then((response) => {
-                if (response.ok) {
-                    console.log('Logged out successfully');
-                     window.location.reload();
-                } else {
-                    console.error('Failed to log out');
-                }
-            })
-            .catch((error) => {
-                console.error('Error during logout:', error);
-            });
+        signOut({callbackUrl: "http://localhost:3000/"});
+        // fetch('/api/logout')
+        //     .then((response) => {
+        //         if (response.ok) {
+        //             console.log('Logged out successfully');
+        //              window.location.reload();
+        //         } else {
+        //             console.error('Failed to log out');
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         console.error('Error during logout:', error);
+        //     });
 
     }
   return (
@@ -46,8 +48,11 @@ export default function NavLinks({isLoggedIn}: Logged) {
         <li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="booking-page" href={"/booking"} onClick={handleLinkClick}>Bookings</Link></li>
         <li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="chat-page" href={"/chat"} onClick={handleLinkClick}>AI Chat</Link></li>
         <li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="recommendations-page" href={"/community"} onClick={handleLinkClick}>Community</Link></li>
-        {!isLoggedIn?<li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="auth-page" href={"/login"} onClick={handleLinkClick}>Login/Signup</Link></li>:
-        <li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="auth-page" href={"/login"} onClick={handleLogout}  >Logout</Link></li>}
+        {/* {!isLoggedIn?<li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="auth-page" href={"/login"} onClick={handleLinkClick}>Login/Signup</Link></li>:
+        <li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="auth-page" href={"/login"} onClick={handleLogout}  >Logout</Link></li>} */}
+        {status!=="authenticated"?<li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="auth-page" href={"/logIn"} onClick={handleLinkClick}>Login/Signup</Link></li>:
+        <li><Link className="nav-btn px-4 py-2 rounded-lg hover:bg-indigo-100 transition-colors duration-200 text-indigo-700 font-medium" data-target="auth-page" href={"/"} onClick={handleLogout}  >Logout</Link></li>}
+
       </ul>
     </>
   )
