8844bd7d0bdf1c3971c51388a878bffefac49c0f
yogesh <yogirao8440@gmail.com>
2025-06-23 09:51:13 +0530

    update: next auth types added

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,23 @@
+import 'next-auth';
+import { DefaultSession } from 'next-auth';
+declare module 'next-auth' {
+
+  interface User {
+    _id?: string;
+    email?: string;
+  }
+
+  interface Session{
+    user: {
+       _id?: string,
+       email?: string
+    }& DefaultSession["user"];
+  } 
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    _id?: string;
+    username?: string;
+  }
+}
