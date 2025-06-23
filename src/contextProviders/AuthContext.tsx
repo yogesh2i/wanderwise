2ddd07d7ea11c2fde0e268f507cdfdecb3601cae
yogesh <yogirao8440@gmail.com>
2025-06-23 09:50:04 +0530

    update: auth context with next auth

--- src/contextProviders/AuthContext.tsx
+++ src/contextProviders/AuthContext.tsx
@@ -1,55 +1,18 @@
 'use client';
 
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { SessionProvider } from 'next-auth/react';
+import React, { ReactNode } from 'react';
 
-interface AuthContextType {
-  isLoggedIn: boolean | false;
-  login: () => void;
-  logout: () => void;
-}
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
-export function AuthProvider({ children }: { children: ReactNode }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(() => {
-    async function checkAuth() {
-        try {
-          const response = await fetch('/api/checkAuth', {
-            method: 'GET',
-            credentials: 'include', 
-          });
-          if (response.ok) {
-            setIsLoggedIn(true);
-          } else {
-            setIsLoggedIn(false);
-          }
-        } catch (error) {
-          console.error('Error checking authentication:', error);
-          setIsLoggedIn(false);
-        }
-      }
+
+export default function AuthProvider({ children }: { children: ReactNode }) {
   
-      checkAuth();
-  }, []);
-
-  const login = () => {
-    setIsLoggedIn(true);
-  };
-
-  const logout = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <SessionProvider>
       {children}
-    </AuthContext.Provider>
+    </SessionProvider>
   );
 }
 
-export function useAuth() {
-  const context = useContext(AuthContext);
-  return context;
-}
